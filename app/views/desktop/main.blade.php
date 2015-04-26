5ef18572b9831b07364929122193bc5be44bd6d1
Mircea <mircea.dinoiu@gmail.com>
2015-04-26 16:18:23 +0300

    Added bootstrap

--- app/views/desktop/main.blade.php
+++ app/views/desktop/main.blade.php
@@ -1,12 +1,25 @@
 <html>
 <head>
     <title>{{ \Lang::get('general.application_title') }}</title>
+
+    {{ HTML::style('desktop/bower_components/bootstrap/dist/css/bootstrap.min.css') }}
+    {{ HTML::style('desktop/bower_components/bootstrap/dist/css/bootstrap-theme.min.css') }}
 </head>
 <body>
     <main>
-        {{ $content }}
+        Loading..
     </main>
     {{ HTML::script('desktop/bower_components/jquery/dist/jquery.min.js') }}
     {{ HTML::script('desktop/bower_components/jquery-pjax/jquery.pjax.js') }}
+    {{ HTML::script('desktop/bower_components/bootstrap/dist/js/bootstrap.min.js') }}
+
+    <script>
+        $(function () {
+            $.pjax({
+                url: location.href,
+                container: 'main'
+            });
+        });
+    </script>
 </body>
 </html>
