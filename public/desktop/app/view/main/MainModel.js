110930eb427247518b670f69ce446c643d8b08b3
Mircea <mircea.dinoiu@gmail.com>
2015-05-23 10:16:45 +0300

    WIP

---
+++ public/desktop/app/view/main/MainModel.js
@@ -0,0 +1,14 @@
+/**
+ * This class is the view model for the Main view of the application.
+ */
+Ext.define('Financial.view.main.MainModel', {
+    extend: 'Ext.app.ViewModel',
+
+    alias: 'viewmodel.main',
+
+    data: {
+        name: 'Financial'
+    }
+
+    //TODO - add data, formulas and/or methods to support your view
+});
---
+++ public/desktop/build/temp/production/Financial/sass/config.rb
@@ -0,0 +1,3 @@
+require '..\..\..\..\..\ext\packages\ext-theme-base\sass\utils.rb'
+Compass.add_project_configuration('..\..\..\..\..\sass\config.rb')
+cache_path = 'D:\OneDrive\Projects\Dinoiu Financial\financial.dinoiu.com\public\desktop/build/.sass-cache'
