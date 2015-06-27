205111a63d42b1b7a193c724c87790c36d888455
Mircea <mircea.dinoiu@gmail.com>
2015-06-27 10:55:28 +0300

    Moving files

--- public/desktop/build/temp/production/Financial/sass/config.rb
+++
@@ -1,3 +0,0 @@
-require '..\..\..\..\..\ext\packages\ext-theme-base\sass\utils.rb'
-Compass.add_project_configuration('..\..\..\..\..\sass\config.rb')
-cache_path = 'D:\OneDrive\Projects\Dinoiu Financial\financial.dinoiu.com\public\desktop/build/.sass-cache'
--- public/desktop/build/temp/production/Financial/sencha-compiler/app/full-page-master-bundle.js
+++
@@ -1,2 +0,0 @@
-// @tag full-page
-// @require D:\OneDrive\Projects\Dinoiu Financial\fncldev.dinoiu.com\public\desktop\app.js
