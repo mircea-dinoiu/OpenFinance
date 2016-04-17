55e4f172a55a36e30e68d0d957b9160462cba60f
root <mircea.dinoiu@softvision.ro>
2016-04-17 08:15:53 +0300

    Fix naming issues

--- app/Models/Income.php
+++ app/Models/Income.php
@@ -5,6 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 
 class Income extends Model {
+    const STATUS_PENDING = 'pending';
+    const STATUS_FINISHED = 'finished';
+
     protected $table = 'incomes';
 
     public function user() {
