97dc89294194df2e3b7096381aa4a790a80fba3a
Mircea Dinoiu <mircea.dinoiu@gmail.com>
2016-12-31 13:26:03 +0200

    Remove mobile app

--- sources/mobile/app/util/Format.js
+++
@@ -1,8 +0,0 @@
-Ext.define('Financial.util.Format', {
-    singleton: true,
-
-    money: function (value) {
-        return value; // todo
-        return Ext.util.Format.number(value, '0,0.00');
-    }
-});
