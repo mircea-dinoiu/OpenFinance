221fec46702dacd70edda03586b98c09b54a098f
Mircea Dinoiu <mircea.dinoiu@gmail.com>
2018-04-02 17:42:13 -0700

    fix breakpoints

--- sources/common/utils/getScreenQueries.js
+++ sources/common/utils/getScreenQueries.js
@@ -1,8 +1,8 @@
 // @flow
 
 export default function getScreenQueries(): TypeScreenQueries {
-    const isSmall = window.matchMedia('(min-device-width : 320px) and (max-device-width : 480px)').matches;
-    const isMedium = window.matchMedia('(min-device-width : 768px) and (max-device-width : 1024px)').matches;
+    const isSmall = window.matchMedia('(min-width: 0px) and (max-width: 480px)').matches;
+    const isMedium = window.matchMedia('(min-width : 768px) and (max-width: 1024px)').matches;
     const isLarge = window.matchMedia('(min-width: 1025px)').matches;
 
     return {
