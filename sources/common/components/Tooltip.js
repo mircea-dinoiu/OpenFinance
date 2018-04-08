03fcb575709a06111ef451401118ebecfff50d99
Mircea Dinoiu <mircea.dinoiu@gmail.com>
2018-04-07 17:12:25 -0700

    add prettier

--- sources/common/components/Tooltip.js
+++ sources/common/components/Tooltip.js
@@ -1,7 +1,8 @@
+// @flow
 import React from 'react';
 import {Tooltip as Tippy} from 'react-tippy';
 
-const Tooltip = ({children, tooltip}) => (
+const Tooltip = ({children, tooltip}: {children: React$Element<any>, tooltip: React$Element<any>}) => (
       <Tippy
           html={
               <div style={{fontSize: '12px', textAlign: 'left'}}>
