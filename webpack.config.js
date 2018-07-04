cc49a37363879e435aefdc0f60122f71b4c17893
Mircea Dinoiu <mircea.dinoiu@gmail.com>
2018-07-04 12:43:06 -0700

    add postcss

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,12 @@
 const path = require('path');
 const webpack = require('webpack');
 const CompressionPlugin = require('compression-webpack-plugin');
-
 const env = process.env.NODE_ENV || 'development';
 
 console.log('Webpack building for', env);
 
 const isProduction = env === 'production';
 const isHot = env === 'hot';
-
 const enableSourceMaps = isProduction === false;
 
 module.exports = {
@@ -16,14 +14,16 @@ module.exports = {
     mode: isProduction ? 'production' : 'development',
     entry: {
         'bundles/Responsive': path.resolve('sources/mobile/index.js'),
-        'bundles/Desktop': path.resolve('sources/desktop/Desktop.js'),
+        'bundles/Desktop': path.resolve('sources/desktop/Desktop.js')
     },
-    devServer: isHot ? {
-        contentBase: path.resolve('./public'),
-        headers: {
-            'Access-Control-Allow-Origin': '*',
+    devServer: isHot
+        ? {
+            contentBase: path.resolve('./public'),
+            headers: {
+                'Access-Control-Allow-Origin': '*'
+            }
         }
-    } : undefined,
+        : undefined,
     output: {
         path: path.resolve('./public/dist'),
         // filename: isProduction ? '[name].[chunkhash].js' : '[name].js',
@@ -34,35 +34,56 @@ module.exports = {
         rules: [
             {
                 test: /\.js$/,
-                exclude: [
-                    /node_modules/
-                ],
+                exclude: [/node_modules/],
                 use: [
-                    {loader: 'babel-loader', options: {cacheDirectory: true}}
+                    {
+                        loader: 'babel-loader',
+                        options: {cacheDirectory: true}
+                    }
                 ].filter(Boolean)
             },
             {
                 test: /\.css$/,
                 use: [
                     {loader: 'style-loader'},
-                    {loader: 'css-loader', options: {sourceMap: enableSourceMaps, url: false}}
+                    {
+                        loader: 'css-loader',
+                        options: {sourceMap: enableSourceMaps, url: false}
+                    }
+                ]
+            },
+            {
+                test: /\.pcss$/,
+                use: [
+                    {loader: 'style-loader'},
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            localIdentName:
+                                '[path][name]__[local]--[hash:base64:5]',
+                            sourceMap: false,
+                            url: false,
+                            modules: true,
+                            importLoaders: 1
+                        }
+                    },
+                    {loader: 'postcss-loader'}
                 ]
             }
         ]
     },
     plugins: [
-        isProduction && new webpack.LoaderOptionsPlugin({
-            minimize: true
-        }),
-        isProduction && new CompressionPlugin({
-            algorithm: 'gzip'
-        }),
-        !isProduction && new webpack.NamedModulesPlugin(),
+        isProduction &&
+            new webpack.LoaderOptionsPlugin({
+                minimize: true
+            }),
+        isProduction &&
+            new CompressionPlugin({
+                algorithm: 'gzip'
+            }),
+        !isProduction && new webpack.NamedModulesPlugin()
     ].filter(Boolean),
     resolve: {
-        modules: [
-            path.resolve('sources'),
-            path.resolve('node_modules')
-        ]
+        modules: [path.resolve('sources'), path.resolve('node_modules')]
     }
 };
