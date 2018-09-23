f572f5693e5a4a5bfe872b8fd13a9b6cc82b5750
Mircea Dinoiu <mircea.dinoiu@gmail.com>
2018-09-23 15:17:41 -0700

    format code

--- sources/common/utils/dates.test.js
+++ sources/common/utils/dates.test.js
@@ -1,17 +1,27 @@
-import {shiftDateForward} from 'common/utils/dates';
-import {formatYMD} from 'shared/utils/dates';
+import { shiftDateForward } from 'common/utils/dates';
+import { formatYMD } from 'shared/utils/dates';
 
 describe('#shiftDateForward()', () => {
     it('should default times to 1', () => {
-        expect(formatYMD(shiftDateForward('2018-01-01', 'm'))).toEqual('2018-02-01');
+        expect(formatYMD(shiftDateForward('2018-01-01', 'm'))).toEqual(
+            '2018-02-01',
+        );
     });
 
     describe('When times is specified', () => {
         it('should shift that many times', () => {
-            expect(formatYMD(shiftDateForward('2018-01-01', 'm', 2))).toEqual('2018-03-01');
-            expect(formatYMD(shiftDateForward('2018-01-01', 'm', 3))).toEqual('2018-04-01');
-            expect(formatYMD(shiftDateForward('2018-01-01', 'm', 4))).toEqual('2018-05-01');
-            expect(formatYMD(shiftDateForward('2018-01-01', 'm', 5))).toEqual('2018-06-01');
+            expect(formatYMD(shiftDateForward('2018-01-01', 'm', 2))).toEqual(
+                '2018-03-01',
+            );
+            expect(formatYMD(shiftDateForward('2018-01-01', 'm', 3))).toEqual(
+                '2018-04-01',
+            );
+            expect(formatYMD(shiftDateForward('2018-01-01', 'm', 4))).toEqual(
+                '2018-05-01',
+            );
+            expect(formatYMD(shiftDateForward('2018-01-01', 'm', 5))).toEqual(
+                '2018-06-01',
+            );
         });
     });
 });
