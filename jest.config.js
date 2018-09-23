22edef5ed35a407ae85252ac61087c603aace143
Mircea Dinoiu <mircea.dinoiu@gmail.com>
2018-09-23 04:12:07 -0700

    remove coverage threshold

--- jest.config.js
+++ jest.config.js
@@ -3,14 +3,6 @@ module.exports = {
     collectCoverage: true,
     coverageReporters: ['text-summary', 'html'],
     coverageDirectory: '<rootDir>/build/coverage',
-    coverageThreshold: {
-        global: {
-            branches: 100,
-            functions: 100,
-            lines: 100,
-            statements: 100,
-        },
-    },
     modulePaths: ['<rootDir>/', '<rootDir>/sources/'],
     moduleNameMapper: {
         '.pcss$': 'test/emptyObjectStub.js',
