d291f12c4cc9824b6b2e9b553fcca597e980e92c
Mircea Dinoiu <mircea.dinoiu@gmail.com>
2019-02-24 22:24:01 -0800

    use background instead backgroundColor to map db entry

--- sources/mobile/ui/internal/expenses/cells/CategoriesDisplay.js
+++ sources/mobile/ui/internal/expenses/cells/CategoriesDisplay.js
@@ -15,7 +15,7 @@ const CategoriesDisplay = ({ item, categories, screen }) => (
                 <Chip
                     key={each.get('id')}
                     style={{
-                        backgroundColor: each.get('color'),
+                        background: each.get('color'),
                         margin: `${screen.isLarge ? 0 : '5px'} 5px 0 0`,
                         ...(screen.isLarge
                             ? {
