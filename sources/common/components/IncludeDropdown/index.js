b65bc60e2963f2c3b11fe7e0942d0f9e1399756e
Mircea Dinoiu <mircea.dinoiu@gmail.com>
2019-08-18 19:29:38 -0700

    better throughout texting

--- sources/common/components/IncludeDropdown/index.js
+++ sources/common/components/IncludeDropdown/index.js
@@ -1,9 +1,64 @@
 // @flow
 import * as React from 'react';
 import {MenuItem, SelectField} from 'material-ui';
-import {IncludeOptions} from './defs';
+import {connect} from 'react-redux';
 
-const IncludeDropdown = ({onChange, value}) => (
+const getIncludeOptions = ({endDate}) => {
+    const currentYear = new Date(endDate).getFullYear();
+
+    return [
+        {
+            id: 'all',
+            name: 'Everything',
+        },
+        {
+            id: 'until-tmrw',
+            name: 'Until tomorrow',
+        },
+        {
+            id: 'ut',
+            name: 'Until today',
+        },
+        {
+            id: 'until-now',
+            name: 'Until now',
+        },
+        {
+            id: 'until-yd',
+            name: 'Until yesterday',
+        },
+        {
+            id: 'ld',
+            name: '1 day before',
+        },
+        {
+            id: 'lw',
+            name: '1 week before',
+        },
+        {
+            id: 'lm',
+            name: '1 month before',
+        },
+        {
+            id: 'ly',
+            name: '1 year before',
+        },
+        {
+            id: 'previous-year',
+            name: `Throughout ${currentYear - 1}`,
+        },
+        {
+            id: 'current-year',
+            name: `Throughout ${currentYear}`,
+        },
+        {
+            id: 'next-year',
+            name: `Throughout ${currentYear + 1}`,
+        },
+    ];
+};
+
+const IncludeDropdown = ({onChange, value, endDate}) => (
     <SelectField
         floatingLabelText="Include results"
         floatingLabelFixed={true}
@@ -12,7 +67,7 @@ const IncludeDropdown = ({onChange, value}) => (
         fullWidth={true}
         style={{margin: '-10px 0 0'}}
     >
-        {IncludeOptions.map((option) => (
+        {getIncludeOptions({endDate}).map((option) => (
             <MenuItem
                 key={option.id}
                 value={option.id}
@@ -22,4 +77,6 @@ const IncludeDropdown = ({onChange, value}) => (
     </SelectField>
 );
 
-export default IncludeDropdown;
+export default connect((state) => ({endDate: state.preferences.endDate}))(
+    IncludeDropdown,
+);
