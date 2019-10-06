b6389f36c71c429b284d8f0ee0271cc32f453651
Mircea Dinoiu <mircea.dinoiu@gmail.com>
2019-10-05 17:57:40 -0700

    improve code

--- sources/common/state/persistency.js
+++ sources/common/state/persistency.js
@@ -1,14 +1,13 @@
 // @flow
 import {parsePreferences} from 'common/utils/preferences';
-import {mapUrlSearchParamsToObject} from 'common/utils';
 import type {TypeGlobalState} from 'common/types';
 
 export const readState = () => {
     try {
         return {
             preferences: parsePreferences(
-                mapUrlSearchParamsToObject(
-                    new URLSearchParams(location.search),
+                Object.fromEntries(
+                    new URLSearchParams(location.search).entries(),
                 ),
             ),
         };
