2e4d4ed58ccc817f8f57352a4debb99e3e98eb32
Mircea Dinoiu <mircea.dinoiu@gmail.com>
2019-10-06 20:36:55 -0700

    internal with hooks

--- sources/mobile/ui/Internal.js
+++ sources/mobile/ui/Internal.js
@@ -1,6 +1,5 @@
 // @flow weak
-import type {TypeScreenQueries} from 'common/types';
-import React, {PureComponent} from 'react';
+import * as React from 'react';
 import {
     BottomNavigation,
     BottomNavigationAction,
@@ -16,30 +15,16 @@ import Summary from './internal/Summary';
 import AccountBalance from '@material-ui/icons/AccountBalance';
 import TrendingDown from '@material-ui/icons/TrendingDown';
 
-import {connect} from 'react-redux';
 import {Col, Row} from 'react-grid-system';
 import {flexColumn} from 'common/defs/styles';
 import {grey} from '@material-ui/core/colors';
+import {useScreenSize} from 'common/state/hooks';
 
-type TypeProps = {
-    screen: TypeScreenQueries,
-};
+const Internal = () => {
+    const [selectedIndex, setSelectedIndex] = React.useState(0);
+    const screenSize = useScreenSize();
 
-type TypeState = {
-    selectedIndex: number,
-    tab: ?React$Element<any>,
-};
-
-class Internal extends PureComponent<TypeProps, TypeState> {
-    state = {
-        selectedIndex: 0,
-        tab: null,
-    };
-    handleChangeTab = (index) => {
-        this.setState({selectedIndex: index});
-    };
-
-    renderMobileTab(index) {
+    const renderMobileTab = (index) => {
         switch (index) {
             case 0:
                 return <Summary />;
@@ -48,115 +33,97 @@ class Internal extends PureComponent<TypeProps, TypeState> {
         }
 
         return null;
-    }
+    };
 
-    renderDesktopTab(index) {
+    const renderComingSoon = () => (
+        <div
+            style={{
+                textAlign: 'center',
+                padding: '100px',
+                fontSize: '48px',
+                color: grey[700],
+            }}
+        >
+            Coming soon
+        </div>
+    );
+
+    const renderDesktopTab = (index) => {
         switch (index) {
             case 0:
                 return <Expenses />;
         }
 
-        return this.renderComingSoon();
-    }
+        return renderComingSoon();
+    };
 
-    componentDidMount() {
-        this.handleChangeTab(this.state.selectedIndex);
-    }
+    const shouldRenderExtraScreens = () => false;
 
-    renderComingSoon() {
-        return (
-            <div
+    const renderLarge = () => (
+        <Row nogutter>
+            <Col xs={2} style={{paddingRight: 0}}>
+                <Summary />
+            </Col>
+            <Col xs={10} style={{paddingLeft: 0}}>
+                <AppBar position="static">
+                    <Tabs
+                        value={selectedIndex}
+                        onChange={(event, index) => setSelectedIndex(index)}
+                    >
+                        <Tab label="Transactions" />
+                        {shouldRenderExtraScreens() && (
+                            <>
+                                <Tab label="Categories" />
+                                <Tab label="Accounts" />
+                                <Tab label="Account Types" />
+                            </>
+                        )}
+                    </Tabs>
+                </AppBar>
+                {renderDesktopTab(selectedIndex)}
+            </Col>
+        </Row>
+    );
+
+    const renderMediumDown = () => (
+        <div
+            style={{
+                paddingBottom: '56px',
+            }}
+        >
+            {renderMobileTab(selectedIndex)}
+            <Paper
+                zDepth={1}
                 style={{
-                    textAlign: 'center',
-                    padding: '100px',
-                    fontSize: '48px',
-                    color: grey[700],
+                    position: 'fixed',
+                    bottom: 0,
+                    zIndex: 1,
+                    width: '100%',
                 }}
             >
-                Coming soon
-            </div>
-        );
-    }
+                <BottomNavigation value={selectedIndex}>
+                    <BottomNavigationAction
+                        label="Summary"
+                        icon={<AccountBalance />}
+                        onClick={() => setSelectedIndex(0)}
+                        onTouchTap={() => setSelectedIndex(0)}
+                    />
+                    <BottomNavigationAction
+                        label="Transactions"
+                        icon={<TrendingDown />}
+                        onClick={() => setSelectedIndex(1)}
+                        onTouchTap={() => setSelectedIndex(1)}
+                    />
+                </BottomNavigation>
+            </Paper>
+        </div>
+    );
 
-    shouldRenderExtraScreens() {
-        return false;
-    }
+    return (
+        <div style={flexColumn}>
+            {screenSize.isLarge ? renderLarge() : renderMediumDown()}
+        </div>
+    );
+};
 
-    renderLarge() {
-        return (
-            <Row nogutter>
-                <Col xs={2} style={{paddingRight: 0}}>
-                    <Summary />
-                </Col>
-                <Col xs={10} style={{paddingLeft: 0}}>
-                    <AppBar position="static">
-                        <Tabs
-                            value={this.state.selectedIndex}
-                            onChange={(event, index) =>
-                                this.handleChangeTab(index)
-                            }
-                        >
-                            <Tab label="Transactions" />
-                            {this.shouldRenderExtraScreens() && (
-                                <>
-                                    <Tab label="Categories" />
-                                    <Tab label="Accounts" />
-                                    <Tab label="Account Types" />
-                                </>
-                            )}
-                        </Tabs>
-                    </AppBar>
-                    {this.renderDesktopTab(this.state.selectedIndex)}
-                </Col>
-            </Row>
-        );
-    }
-
-    renderMediumDown() {
-        return (
-            <div
-                style={{
-                    paddingBottom: '56px',
-                }}
-            >
-                {this.renderMobileTab(this.state.selectedIndex)}
-                <Paper
-                    zDepth={1}
-                    style={{
-                        position: 'fixed',
-                        bottom: 0,
-                        zIndex: 1,
-                        width: '100%',
-                    }}
-                >
-                    <BottomNavigation value={this.state.selectedIndex}>
-                        <BottomNavigationAction
-                            label="Summary"
-                            icon={<AccountBalance />}
-                            onClick={() => this.handleChangeTab(0)}
-                            onTouchTap={() => this.handleChangeTab(0)}
-                        />
-                        <BottomNavigationAction
-                            label="Transactions"
-                            icon={<TrendingDown />}
-                            onClick={() => this.handleChangeTab(1)}
-                            onTouchTap={() => this.handleChangeTab(1)}
-                        />
-                    </BottomNavigation>
-                </Paper>
-            </div>
-        );
-    }
-
-    render() {
-        return (
-            <div style={flexColumn}>
-                {this.props.screen.isLarge
-                    ? this.renderLarge()
-                    : this.renderMediumDown()}
-            </div>
-        );
-    }
-}
-
-export default connect(({screen}) => ({screen}))(Internal);
+export default Internal;
