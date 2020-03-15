acfabf9ebe3b7ef94a8bfc9e470a198d3dc44eb0
Mircea Dinoiu <mircea.dinoiu@gmail.com>
2020-03-15 02:45:39 -0700

    change ui for description filter

--- src/components/BaseTable/filters/DescriptionFilter.tsx
+++ src/components/BaseTable/filters/DescriptionFilter.tsx
@@ -1,13 +1,10 @@
 import * as React from 'react';
 import {SyntheticEvent} from 'react';
 import {IconButton, IconMenu, RadioButton, RadioButtonGroup} from 'material-ui';
-import {
-    DepositFlag,
-    GeneratedFlag,
-    PendingReviewFlag,
-    RecurrentFlag,
-} from 'components/internal/common/MainScreenFlags';
+import IconFilterList from '@material-ui/icons/FilterList';
 import {DebounceInput} from 'react-debounce-input';
+import {gridGap, spacingLarge} from 'defs/styles';
+import styled from 'styled-components';
 
 const PENDING = 'Pending';
 const RECURRENT = 'Recurrent';
@@ -16,36 +13,33 @@ const DEPOSIT = 'Deposit';
 const YES = 'yes';
 const NO = 'no';
 const ONLY = 'only';
-const FlagIconMenu = ({icon, filter, name, onChange}) => (
-    <IconMenu
-        iconButtonElement={
-            <IconButton style={{padding: 0, width: 'auto', height: 'auto'}}>
-                {icon}
-            </IconButton>
-        }
-        style={{position: 'relative', top: 5}}
-        anchorOrigin={{horizontal: 'left', vertical: 'top'}}
-        targetOrigin={{horizontal: 'left', vertical: 'top'}}
-    >
-        <div style={{padding: '0 10px'}}>
-            Display {name} Transactions
-            <RadioButtonGroup
-                name={name}
-                defaultSelected={
-                    (filter && filter.value && filter.value[name]) || YES
-                }
-                style={{margin: '10px 0 0'}}
-                onChange={(event: SyntheticEvent<HTMLInputElement>) =>
-                    onChange(name, event.currentTarget.value)
-                }
-            >
-                <RadioButton value={YES} label="Yes" />
-                <RadioButton value={NO} label="No" />
-                <RadioButton value={ONLY} label="Exclusive" />
-            </RadioButtonGroup>
-        </div>
-    </IconMenu>
+const Subfilter = ({filter, name, onChange}) => (
+    <div>
+        Display {name} Transactions
+        <RadioButtonGroup
+            name={name}
+            defaultSelected={
+                (filter && filter.value && filter.value[name]) || YES
+            }
+            style={{margin: '10px 0 0'}}
+            onChange={(event: SyntheticEvent<HTMLInputElement>) =>
+                onChange(name, event.currentTarget.value)
+            }
+        >
+            <RadioButton value={YES} label="Yes" />
+            <RadioButton value={NO} label="No" />
+            <RadioButton value={ONLY} label="Exclusive" />
+        </RadioButtonGroup>
+    </div>
 );
+
+const SubfilterGrid = styled.div`
+    display: grid;
+    grid-gap: ${gridGap};
+    grid-template-columns: 1fr 1fr;
+    grid-template-rows: 1fr 1fr;
+    padding: 0 ${spacingLarge};
+`;
 
 export const DescriptionFilter = ({onChange, filter}) => {
     const handleChange = (name, value) => {
@@ -58,30 +52,42 @@ export const DescriptionFilter = ({onChange, filter}) => {
 
     return (
         <div style={{textAlign: 'left', display: 'flex', flexDirection: 'row'}}>
-            <FlagIconMenu
-                onChange={handleChange}
-                name={DEPOSIT}
-                filter={filter}
-                icon={<DepositFlag />}
-            />
-            <FlagIconMenu
-                onChange={handleChange}
-                name={PENDING}
-                filter={filter}
-                icon={<PendingReviewFlag />}
-            />
-            <FlagIconMenu
-                onChange={handleChange}
-                name={RECURRENT}
-                filter={filter}
-                icon={<RecurrentFlag />}
-            />
-            <FlagIconMenu
-                onChange={handleChange}
-                name={GENERATED}
-                filter={filter}
-                icon={<GeneratedFlag />}
-            />
+            <IconMenu
+                iconButtonElement={
+                    <IconButton
+                        style={{padding: 0, width: 'auto', height: 'auto'}}
+                    >
+                        <IconFilterList />
+                    </IconButton>
+                }
+                style={{position: 'relative', top: 5}}
+                anchorOrigin={{horizontal: 'left', vertical: 'top'}}
+                targetOrigin={{horizontal: 'left', vertical: 'top'}}
+            >
+                <SubfilterGrid>
+                    <Subfilter
+                        onChange={handleChange}
+                        name={DEPOSIT}
+                        filter={filter}
+                    />
+                    <Subfilter
+                        onChange={handleChange}
+                        name={PENDING}
+                        filter={filter}
+                    />
+                    <Subfilter
+                        onChange={handleChange}
+                        name={RECURRENT}
+                        filter={filter}
+                    />
+                    <Subfilter
+                        onChange={handleChange}
+                        name={GENERATED}
+                        filter={filter}
+                    />
+                </SubfilterGrid>
+            </IconMenu>
+
             <DebounceInput
                 placeholder="Type to search..."
                 type="search"
