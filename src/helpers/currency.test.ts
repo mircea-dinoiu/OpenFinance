89817f7217f5b4a03f678e06126f1748af9ab657
Mircea Dinoiu <mircea.dinoiu@gmail.com>
2020-03-15 15:29:23 -0700

    test

--- src/helpers/currency.test.ts
+++ src/helpers/currency.test.ts
@@ -20,26 +20,23 @@ const USD = {
     rates: {RON: 3.9824, EUR: 0.8542, GBP: 0.7558},
 };
 const currencies = {
-    map: {
-        '1': USD,
-        '2': RON,
-        '3': {
-            id: 3,
-            iso_code: 'EUR',
-            currency: 'Euro',
-            symbol: '€',
-            rates: {RON: 4.6621, GBP: 0.8848, USD: 1.1706},
-        },
-        '4': {
-            id: 4,
-            iso_code: 'GBP',
-            currency: 'Pound sterling',
-            symbol: '£',
-            rates: {RON: 5.2691, EUR: 1.1301, USD: 1.3231},
-        },
+    '1': USD,
+    '2': RON,
+    '3': {
+        id: 3,
+        iso_code: 'EUR',
+        currency: 'Euro',
+        symbol: '€',
+        rates: {RON: 4.6621, GBP: 0.8848, USD: 1.1706},
     },
-    default: 1,
-    from_cache: false,
+    '4': {
+        id: 4,
+        iso_code: 'GBP',
+        currency: 'Pound sterling',
+        symbol: '£',
+        rates: {RON: 5.2691, EUR: 1.1301, USD: 1.3231},
+    },
+    selected: USD,
 };
 
 describe('#findCurrencyById()', () => {
