b78273eeca1d2b4b047ab3d340a6c9bd76f1112e
Mircea Dinoiu <mircea.dinoiu@gmail.com>
2020-03-28 00:41:12 -0700

    more upgrades

--- src/components/transactions/SummaryCategory.tsx
+++ src/components/transactions/SummaryCategory.tsx
@@ -3,11 +3,15 @@ import {convertCurrencyToDefault} from 'helpers/currency';
 import React from 'react';
 import groupBy from 'lodash/groupBy';
 import sortBy from 'lodash/sortBy';
-import {Card, CardHeader, CardText} from 'material-ui';
+import {Card, CardHeader, IconButton} from '@material-ui/core';
 import {financialNum} from 'js/utils/numbers';
 import {SummarySubCategory} from 'components/transactions/SummarySubCategory';
 import {numericValue} from 'components/formatters';
 import {useCurrencies} from 'state/currencies';
+import ExpandMore from '@material-ui/icons/ExpandMore';
+import ExpandLess from '@material-ui/icons/ExpandLess';
+import {makeStyles} from '@material-ui/core/styles';
+import {spacingSmall} from 'defs/styles';
 
 const groupSorter = ([, items]) => {
     if (items.length > 0) {
@@ -28,7 +32,26 @@ const groupSorter = ([, items]) => {
 
 export const headerColor = 'rgba(255, 255, 255, 0.9)';
 
+const useStyles = makeStyles({
+    expandable: {
+        padding: spacingSmall,
+    },
+});
+const useCardHeaderStyles = makeStyles({
+    title: {
+        fontSize: 'medium',
+    },
+    subheader: {
+        fontSize: 'small',
+    },
+    action: {
+        margin: 0,
+    },
+});
+
 export const SummaryCategory = (props) => {
+    const cls = useStyles();
+    const cardHeaderClasses = useCardHeaderStyles();
     const currencies = useCurrencies();
     const {
         backgroundColor,
@@ -66,20 +89,19 @@ export const SummaryCategory = (props) => {
     };
 
     return (
-        <Card
-            style={{marginBottom: 10}}
-            expanded={expanded}
-            onExpandChange={setExpanded}
-        >
+        <Card style={{marginBottom: 10}}>
             <CardHeader
-                style={{backgroundColor, paddingTop: 0}}
-                actAsExpander={true}
-                showExpandableButton={true}
-            >
-                <div style={{color: headerColor}}>
-                    {title}
-                    {showSumInHeader && (
-                        <div style={{fontSize: '12px'}}>
+                style={{backgroundColor, color: headerColor}}
+                title={title}
+                classes={cardHeaderClasses}
+                action={
+                    <IconButton onClick={() => setExpanded(!expanded)}>
+                        {expanded ? <ExpandLess /> : <ExpandMore />}
+                    </IconButton>
+                }
+                subheader={
+                    showSumInHeader && (
+                        <div style={{color: headerColor}}>
                             {numericValueProxy(
                                 financialNum(
                                     summaryObject.reduce(
@@ -98,39 +120,43 @@ export const SummaryCategory = (props) => {
                                 {currencyStyle: {color: headerColor}},
                             )}
                         </div>
-                    )}
-                </div>
+                    )
+                }
+            >
+                <div style={{color: headerColor}}>{title}</div>
             </CardHeader>
 
-            <CardText style={{padding: '0 5px'}} expandable={true}>
-                {sortBy(
-                    Object.entries(groupBy(summaryObject, 'group')),
-                    groupSorter,
-                ).map(([id, items]) => {
-                    const shouldGroup = items.every((each) =>
-                        each.hasOwnProperty('group'),
-                    );
+            {expanded && (
+                <div className={cls.expandable}>
+                    {sortBy(
+                        Object.entries(groupBy(summaryObject, 'group')),
+                        groupSorter,
+                    ).map(([id, items]) => {
+                        const shouldGroup = items.every((each) =>
+                            each.hasOwnProperty('group'),
+                        );
 
-                    return (
-                        <SummarySubCategory
-                            key={id}
-                            excluded={excluded}
-                            shouldGroup={shouldGroup}
-                            id={id}
-                            items={items}
-                            // @ts-ignore
-                            numericValueFn={numericValueProxy}
-                            entities={entities}
-                            entityIdField={entityIdField}
-                            entityNameField={entityNameField}
-                            expandedByDefault={expandedByDefault}
-                            currencies={currencies}
-                            onToggleExcluded={handleToggleExcluded}
-                            renderDescription={props.renderDescription}
-                        />
-                    );
-                })}
-            </CardText>
+                        return (
+                            <SummarySubCategory
+                                key={id}
+                                excluded={excluded}
+                                shouldGroup={shouldGroup}
+                                id={id}
+                                items={items}
+                                // @ts-ignore
+                                numericValueFn={numericValueProxy}
+                                entities={entities}
+                                entityIdField={entityIdField}
+                                entityNameField={entityNameField}
+                                expandedByDefault={expandedByDefault}
+                                currencies={currencies}
+                                onToggleExcluded={handleToggleExcluded}
+                                renderDescription={props.renderDescription}
+                            />
+                        );
+                    })}
+                </div>
+            )}
         </Card>
     );
 };
