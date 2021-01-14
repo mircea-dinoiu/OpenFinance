9f4fc4ec677cf375a29cdb87c1405061468b987d
Mircea Dinoiu <mircea.dinoiu@gmail.com>
2021-01-14 00:32:01 -0800

    infer stock prices from transactions

--- server/crons/updateStocks.js
+++ server/crons/updateStocks.js
@@ -1,4 +1,4 @@
-const {Stock} = require('../models');
+const {Stock, Expense} = require('../models');
 const finnhub = require('finnhub');
 const logger = require('../helpers/logger');
 
@@ -8,18 +8,14 @@ const finnhubClient = new finnhub.DefaultApi();
 module.exports = async () => {
     const models = await Stock.findAll();
 
-    models.forEach((model) => {
-        if (model.type === 'custom') {
-            return;
-        }
-
-        finnhubClient.quote(model.symbol, (error, data, response) => {
+    models.forEach(async (model) => {
+        finnhubClient.quote(model.symbol, async (error, data, response) => {
             if (!error) {
                 const {c: price} = data;
 
                 if (model.price !== price) {
                     if (price === 0) {
-                        logger.log('FINNHUB', `Unable to find ${model.symbol}`, data);
+                        inferStockPriceFromTransactions(model);
                     } else {
                         logger.log('FINNHUB', `Updating ${model.symbol} price: ${model.price} → ${price}`);
                         model.update({price});
@@ -30,4 +26,26 @@ module.exports = async () => {
             }
         });
     });
+};
+
+const inferStockPriceFromTransactions = async (stock) => {
+    const transaction = await Expense.findOne({
+        where: {
+            stock_id: stock.id,
+        },
+        order: [['created_at', 'DESC']],
+    });
+
+    if (transaction) {
+        const price = transaction.price;
+
+        if (price === stock.price) {
+            return;
+        }
+
+        logger.log('Stocks', `Updating ${stock.symbol} price ${stock.price} → ${price}`);
+        stock.update({price});
+    } else {
+        logger.log('Stocks', `Unable to price ${stock.symbol}`);
+    }
 };
