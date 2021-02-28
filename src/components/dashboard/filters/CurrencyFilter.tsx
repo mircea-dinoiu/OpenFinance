f99956b43907f33d1326314fbb13fff7a02e4254
Mircea Dinoiu <mircea.dinoiu@gmail.com>
2021-02-28 11:19:37 -0800

    dont display currency filter if theres less than two currencies

--- src/components/dashboard/filters/CurrencyFilter.tsx
+++ src/components/dashboard/filters/CurrencyFilter.tsx
@@ -15,16 +15,24 @@ export const CurrencyFilter = <Id extends number | string>({
     const currenciesMap = useCurrenciesMap();
 
     return (
-        <FormControl>
-            <FormLabel>Currency</FormLabel>
-            <RadioGroup value={String(selected)} onChange={(e) => onChange(e.target.value as Id)}>
-                {sortBy(
-                    ids.map((currencyId) => currenciesMap[currencyId as string]),
-                    'iso_code',
-                ).map((currency) => (
-                    <FormControlLabel value={String(currency.id)} control={<Radio />} label={currency.iso_code} />
-                ))}
-            </RadioGroup>
-        </FormControl>
+        <>
+            {ids.length > 1 && (
+                <FormControl>
+                    <FormLabel>Currency</FormLabel>
+                    <RadioGroup value={String(selected)} onChange={(e) => onChange(e.target.value as Id)}>
+                        {sortBy(
+                            ids.map((currencyId) => currenciesMap[currencyId as string]),
+                            'iso_code',
+                        ).map((currency) => (
+                            <FormControlLabel
+                                value={String(currency.id)}
+                                control={<Radio />}
+                                label={currency.iso_code}
+                            />
+                        ))}
+                    </RadioGroup>
+                </FormControl>
+            )}
+        </>
     );
 };
