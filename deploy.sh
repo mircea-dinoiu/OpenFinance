7f6c188e1a91d89a36acb3516902dde951a4f972
Mircea Dinoiu <mircea.dinoiu@gmail.com>
2021-04-26 01:54:33 -0700

    verbose

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,10 @@
-   git checkout master
-   git pull origin master
-   yarn
-   yarn build
-   eb deploy
+echo '- Checking out...'
+git checkout master
+echo '- Pulling latest master...'
+git pull origin master
+echo '- Installing missing node_modules...'
+yarn
+echo '- Building React App...'
+yarn build
+echo '- Deploying to EBS...'
+eb deploy
