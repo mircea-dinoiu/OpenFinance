8580b2ee3f8c8744c8f250d54180ecfb78d3367f
Mircea Dinoiu <mircea.dinoiu@gmail.com>
2021-06-15 03:41:23 -0700

    Redesign end date picker

--- src/transactions/TransactionsEndDatePicker.tsx
+++ src/transactions/TransactionsEndDatePicker.tsx
@@ -1,22 +1,22 @@
-import {FormLabel, IconButton, Menu, MenuItem, InputAdornment, Button, Divider} from '@material-ui/core';
+import {Menu, MenuItem, Button, Divider, TextField, Typography} from '@material-ui/core';
 import {styled} from '@material-ui/core/styles';
 import {DatePicker} from '@material-ui/pickers';
 import {ShiftDateOptions} from 'app/dates/defs';
 import {shiftDateBack, shiftDateForward, useEndDate, endOfDayToISOString} from 'app/dates/helpers';
-import {ShiftMenu, ShiftMenuToday} from 'app/topBar/ShiftMenu';
+import {EndDatePickerMenuGrid, ShiftMenuToday, ShiftMenu} from 'app/topBar/ShiftMenu';
 import moment from 'moment';
 import * as React from 'react';
 import {useHistory, useLocation} from 'react-router-dom';
 import {useBootstrap} from 'users/state';
-import {MoreVert as IconMoreVert} from '@material-ui/icons';
+import {useIsDesktop} from '../app/useIsDesktop';
 
 export const TransactionsEndDatePicker = () => {
     const history = useHistory();
     const location = useLocation();
     const [endDate, setEndDate] = useEndDate();
-    const [showShiftMenu, setShowShiftMenu] = React.useState(false);
     const [shiftMenuAnchor, setShiftMenuAnchor] = React.useState<Element | null>(null);
     const user = useBootstrap();
+    const isDesktop = useIsDesktop();
 
     const setDate = (date: Date) => {
         const searchParams = new URLSearchParams(location.search);
@@ -30,81 +30,8 @@ export const TransactionsEndDatePicker = () => {
     };
 
     const handleCloseShiftMenu = () => {
-        setShowShiftMenu(false);
+        setShiftMenuAnchor(null);
     };
-
-    const renderShiftMenu = () => (
-        <Menu open={showShiftMenu} anchorEl={shiftMenuAnchor} onClose={handleCloseShiftMenu}>
-            <ShiftMenu>
-                <div>
-                    <FormLabel component="legend">Back</FormLabel>
-                    {renderMenuItem({
-                        label: 'Month Start',
-                        getDate: () =>
-                            moment(endDate)
-                                .set({
-                                    date: 1,
-                                })
-                                .toDate(),
-                    })}
-                    {renderMenuItem({
-                        label: 'Year Start',
-                        getDate: () =>
-                            moment(endDate)
-                                .set({
-                                    date: 1,
-                                    month: 0,
-                                })
-                                .toDate(),
-                    })}
-                    {ShiftDateOptions.map((sdo) =>
-                        renderMenuItem({
-                            label: `-${sdo.label}`,
-                            getDate: () => shiftDateBack(endDate, sdo.value),
-                        }),
-                    )}
-                </div>
-                <div>
-                    <FormLabel component="legend">Forward</FormLabel>
-                    {renderMenuItem({
-                        label: 'Month End',
-                        getDate: () => {
-                            const m = moment(endDate);
-
-                            return m
-                                .set({
-                                    date: 0,
-                                    months: m.month() + 1,
-                                })
-                                .toDate();
-                        },
-                    })}
-                    {renderMenuItem({
-                        label: 'Year End',
-                        getDate: () =>
-                            moment(endDate)
-                                .set({
-                                    date: 31,
-                                    months: 11,
-                                })
-                                .toDate(),
-                    })}
-                    {ShiftDateOptions.map((sdo) =>
-                        renderMenuItem({
-                            label: `+${sdo.label}`,
-                            getDate: () => shiftDateForward(endDate, sdo.value),
-                        }),
-                    )}
-                </div>
-            </ShiftMenu>
-            <Divider />
-            <ShiftMenuToday>
-                <Button fullWidth={true} variant="outlined" onClick={() => setDate(new Date())}>
-                    Today
-                </Button>
-            </ShiftMenuToday>
-        </Menu>
-    );
 
     const renderMenuItem = ({label, getDate}: {label: string; getDate: () => Date}) => {
         return (
@@ -119,47 +46,130 @@ export const TransactionsEndDatePicker = () => {
             </MenuItem>
         );
     };
+    const datePickerValue = endDate ? moment(endDate).toDate() : null;
 
     return (
         user && (
             <>
                 <Container>
-                    <DatePicker
-                        fullWidth={true}
-                        variant="inline"
-                        format={'L'}
-                        value={endDate ? moment(endDate).toDate() : null}
-                        onChange={(date) => {
-                            setEndDate(endOfDayToISOString(date as any));
-                        }}
+                    <TextField
                         InputProps={{
-                            endAdornment: (
-                                <InputAdornment position="end">
-                                    <IconButton
-                                        edge="end"
-                                        onClick={(e) => {
-                                            e.preventDefault();
-                                            e.stopPropagation();
-
-                                            setShowShiftMenu(true);
-                                            setShiftMenuAnchor(e.currentTarget);
-                                        }}
-                                    >
-                                        <IconMoreVert />
-                                    </IconButton>
-                                </InputAdornment>
-                            ),
+                            readOnly: true,
                         }}
-                        onContextMenu={(event) => {
-                            event.preventDefault();
-                            event.stopPropagation();
+                        value={datePickerValue ? moment(datePickerValue).format('L') : ''}
+                        onClick={(e) => {
+                            e.preventDefault();
+                            e.stopPropagation();
 
-                            setShowShiftMenu(true);
-                            setShiftMenuAnchor(event.currentTarget);
+                            if (shiftMenuAnchor) {
+                                return;
+                            }
+
+                            setShiftMenuAnchor(e.currentTarget);
                         }}
                     />
                 </Container>
-                {renderShiftMenu()}
+                <Menu
+                    open={!!shiftMenuAnchor}
+                    anchorEl={shiftMenuAnchor}
+                    onClose={handleCloseShiftMenu}
+                    style={{
+                        marginTop: '30px',
+                    }}
+                    MenuListProps={{
+                        disablePadding: true,
+                    }}
+                >
+                    <EndDatePickerMenuGrid>
+                        <DatePicker
+                            fullWidth={true}
+                            variant="static"
+                            format={'L'}
+                            value={datePickerValue}
+                            onChange={(date) => {
+                                setEndDate(endOfDayToISOString(date as any));
+                            }}
+                        />
+                        {isDesktop && (
+                            <div>
+                                <ShiftMenu>
+                                    <div>
+                                        <Typography variant="h6" align="center" paragraph={true}>
+                                            Back
+                                        </Typography>
+
+                                        {renderMenuItem({
+                                            label: 'Month Start',
+                                            getDate: () =>
+                                                moment(endDate)
+                                                    .set({
+                                                        date: 1,
+                                                    })
+                                                    .toDate(),
+                                        })}
+                                        {renderMenuItem({
+                                            label: 'Year Start',
+                                            getDate: () =>
+                                                moment(endDate)
+                                                    .set({
+                                                        date: 1,
+                                                        month: 0,
+                                                    })
+                                                    .toDate(),
+                                        })}
+                                        {ShiftDateOptions.map((sdo) =>
+                                            renderMenuItem({
+                                                label: `-${sdo.label}`,
+                                                getDate: () => shiftDateBack(endDate, sdo.value),
+                                            }),
+                                        )}
+                                    </div>
+                                    <div>
+                                        <Typography variant="h6" align="center" paragraph={true}>
+                                            Forward
+                                        </Typography>
+
+                                        {renderMenuItem({
+                                            label: 'Month End',
+                                            getDate: () => {
+                                                const m = moment(endDate);
+
+                                                return m
+                                                    .set({
+                                                        date: 0,
+                                                        months: m.month() + 1,
+                                                    })
+                                                    .toDate();
+                                            },
+                                        })}
+                                        {renderMenuItem({
+                                            label: 'Year End',
+                                            getDate: () =>
+                                                moment(endDate)
+                                                    .set({
+                                                        date: 31,
+                                                        months: 11,
+                                                    })
+                                                    .toDate(),
+                                        })}
+                                        {ShiftDateOptions.map((sdo) =>
+                                            renderMenuItem({
+                                                label: `+${sdo.label}`,
+                                                getDate: () => shiftDateForward(endDate, sdo.value),
+                                            }),
+                                        )}
+                                    </div>
+                                </ShiftMenu>
+                                <Divider />
+                                <ShiftMenuToday>
+                                    <Button fullWidth={true} variant="outlined" onClick={() => setDate(new Date())}>
+                                        Today
+                                    </Button>
+                                </ShiftMenuToday>
+                            </div>
+                        )}
+                    </EndDatePickerMenuGrid>
+                </Menu>
             </>
         )
     );
@@ -170,6 +180,9 @@ const Container = styled('div')(({theme}) => ({
     gridAutoFlow: 'column',
     gridGap: theme.spacing(1),
     justifyItems: 'center',
+    '& input': {
+        textAlign: 'center',
+    },
     [theme.breakpoints.up('lg')]: {
         alignItems: 'center',
     },
